7af598472b7ec7a6c9d51bfac4870b099d86ff25
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-09-28 18:02:58 +0300

    Delete basic_operations.py

--- basic_operations.py
+++
@@ -1,5 +0,0 @@
-number1=10
-number2=5
-print("Addition of",number1,"and",number2,"is",number1 + number2)
-print("Subtraction of",number1,"and",number2,"is",number1 - number2)
-print("Multiplication of",number1,"and",number2,"is",number1 * number2)
