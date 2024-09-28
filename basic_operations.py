8a8fdd4bf4140bb7a55e67f5cdb3e6b40d91eb74
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-09-28 17:57:51 +0300

    basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,5 +1,5 @@
 number1=10
 number2=5
-print("addition of",number1,"and",number2,"is",number1+number2)
-print("subtraction of",number1,"and",number2,"is",number1-number2)
-print("multiplication of",number1,"and",number2,"is",number1*number2)
+print("Addition of",number1,"and",number2,"is",number1 + number2)
+print("Subtraction of",number1,"and",number2,"is",number1 - number2)
+print("Multiplication of",number1,"and",number2,"is",number1 * number2)
