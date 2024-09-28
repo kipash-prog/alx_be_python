7045ded9d83d23e1a725e3fff61f5aa96684e7bc
kipash-prog <kidus.ugr-0110-15@aau.edu.et>
2024-09-28 17:46:45 +0300

    introduction to python

---
+++ basic_operations.py
@@ -0,0 +1,5 @@
+number1=10
+number2=5
+print("addition of",number1,"and",number2,"is",number1+number2)
+print("subtraction of",number1,"and",number2,"is",number1-number2)
+print("multiplication of",number1,"and",number2,"is",number1*number2)
