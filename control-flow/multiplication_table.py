0295fc591bf82596d3e3421b5fa59ce9a9776197
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-09-29 17:54:17 +0300

    multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = int(input("Enter a number to see its multiplication table:"))
+number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1,11):
     x=number
     y=i
