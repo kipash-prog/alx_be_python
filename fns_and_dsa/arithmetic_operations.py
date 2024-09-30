6d2ce02462701536ffc1d910eb7e00ed87b5c597
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-09-30 18:52:14 +0300

    arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,13 @@
-def perform_operation(num1,num2,operation):
-    if operation=="add":
-       return num2+num1
-    if operation=="subtract":
-       return num2+num1
-    if operation=="multiply":
-       return num1*num2
-    if operation=="divide":
-        if(num2 !=0):
-          return num1/num2
-        else:
-           print("can't divided zero.")
-        
-
-
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero is not allowed"
+        return num1 / num2
+    else:
+        return "Error: Invalid operation"
