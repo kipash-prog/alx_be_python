76221852b664769ad42a7bb4e28ba37b1d0a0098
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-09-30 23:58:29 +0300

    shopping list

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,33 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add_item=input("Enter item:")
+            shopping_list.append(add_item)
+        elif choice == '2':
+            remove_item=input("Enter item:")
+            if remove_item in shopping_list:
+              shopping_list.remove(remove_item)
+              print(f"'{remove_item}' has been removed from your shopping list.")
+            else:
+                print(f"{remove_item} is not found in the list")
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
