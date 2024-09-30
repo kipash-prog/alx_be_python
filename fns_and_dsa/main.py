bf8c5909c3e67fb6997e85c8d42e9f8a74762224
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-09-30 18:51:48 +0300

    main.py

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -5,6 +5,7 @@ def main():
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
     result = perform_operation(num1, num2, operation)
     print(f"Result: {result}")
 
