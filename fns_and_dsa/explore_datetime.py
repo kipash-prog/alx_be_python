339c804dd8b79b6b79c6973dbaf8a105579266c2
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-01 00:55:22 +0300

    Update explore_datetime.py

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,10 +6,9 @@ def display_current_datetime():
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))   
     current_date=datetime.datetime.now().date()
-    future_date = current_date + datetime.timedelta(days=number_of_days)
+    future_date = current_date + datetime.timedelta(days=number_of_days, '%Y-%m-%d\s*%H:%M:%S')
     print(f"Future date: {future_date}")
 if __name__ == "__main__":
     display_current_datetime()
     calculate_future_date()   
  
-#%Y-%m-%d\s*%H:%M:%S
