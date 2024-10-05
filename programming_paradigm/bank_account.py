6cfeb6b0c7c28701b19d6bb9083eb059caab2f4e
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-06 01:18:12 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,16 @@
 class BankAccount:
-    def __init__(self, account_balance: int = 0):
-        self.account_balance=account_balance
-        
-    def deposit(self,amount):
-       self.account_balance += amount
-    
-    def withdraw(self,amount):
-        if self.account_balance >=amount:
-          self.account_balance -=amount
-          return True  
+    def __init__(self, account_balance: int = 0): 
+        self.account_balance = account_balance 
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True  
         else:
-         return False
+            return False
+
     def display_balance(self):
-        print(f""Current Balance:": {self.account_balance}")
-
-
+        print(f"Current Balance: {self.account_balance}")
