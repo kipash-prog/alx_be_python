5b1ac0042c435fbb773a0d8264cd8e6ee159a5bf
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-06 01:06:07 +0300

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self,account_balance:int):
+        self.account_balance=0
+        
+    def deposit(self,amount):
+       self.account_balance += amount
+    
+    def withdraw(self,amount):
+        if self.account_balance >=amount:
+          self.account_balance -=amount
+          return True  
+        else:
+         return False
+    def display_balance(self):
+        print(f"dear customer your current balncce is: {self.account_balance}")
+
+
