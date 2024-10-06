0e78be4be279df0b022e13b8d8fc57632a38b82e
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-06 20:10:19 +0300

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,10 +1,12 @@
-
 def safe_divide(numerator, denominator):
     try:
-        result = flaot(numerator) / float(denominator)
-        print(f"The result of the division is {result}")
+        numerator = float(numerator)
+        denominator = float(denominator)
+        
+        result = numerator / denominator
+        return f"The result of the division is {result}"
     
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        return "Error: Cannot divide by zero."
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Both inputs must be numbers."
