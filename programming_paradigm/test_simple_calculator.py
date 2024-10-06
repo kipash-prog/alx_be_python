1559cf5e319bce8d8a0ceb792c7febb0c6cb01e4
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-06 20:31:31 +0300

    simple calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,21 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+    def test_addition(self):
+        self.assertEqual(self.calc.add(5,10),15)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5,10),-5)
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(5,10),50)
+    def test_divison(self):
+        self.assertEqual(self.calc.divide(10,5),2)
+        
+if __name__ == "__main__":
+    unittest.main()
+    
+
