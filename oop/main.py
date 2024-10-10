69f7001b3777da9c4d247c23e3f3defff5674bbd
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-10 15:59:37 +0300

    Update main.py

--- oop/main.py
+++ oop/main.py
@@ -7,10 +7,7 @@ def main():
     digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
     paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
-    my_library.add_book(classic_book)
-    my_library.add_book(digital_novel)
-    my_library.add_book(paper_novel)
-
+   
     my_library.list_books()
 
 if __name__ == "__main__":
