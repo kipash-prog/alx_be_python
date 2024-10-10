df84111c3184a33009fa73c21136d6cd1e8bcdee
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-10-10 16:47:05 +0300

    Update main.py

--- oop/main.py
+++ oop/main.py
@@ -4,12 +4,6 @@ def main():
     my_library = Library()
 
     classic_book = Book("Pride and Prejudice", "Jane Austen")
-    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500from library_system import Book, EBook, PrintBook, Library
-
-def main():
-    my_library = Library()
-
-    classic_book = Book("Pride and Prejudice", "Jane Austen")
     digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
     paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
@@ -18,13 +12,6 @@ def main():
     my_library.add_book(paper_novel)
 
     my_library.list_books()
-
-if __name__ == "__main__":
-    main()
-)
-    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
-
-   
 
 if __name__ == "__main__":
     main()
